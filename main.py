a146657291eef3d337a681e7600a25bd6baab6c0
Yanghuiwon22 <127187225+Yanghuiwon22@users.noreply.github.com>
2024-04-15 12:23:31 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,53 +1,75 @@
 import pygame
 
+
+class Camera():
+    def __init__(self):
+        self.offset = pygame.math.Vector2(0, 0)
+        self.speed = 4
+
+
 pygame.init()
 
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 500
-
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 pygame.display.set_caption("final_py_chamber")
+background = pygame.image.load('./background.png')
+
+# 캐릭터 세팅하기
+character = pygame.image.load('./character_ch.png')
+character_size = character.get_size()
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = SCREEN_WIDTH/2 - character_width/2
+character_y_pos = SCREEN_HEIGHT/2 - character_height/2
 
 # 화면의 아이콘 정하기
 # widowicon = pygame.image.load("icon.png")
-#
 # pygame.display.set_icon(windowicon)
+
 clock = pygame.time.Clock()
+camera = Camera()
 
 playing = True
 while playing:
-    clock.tick(60)
-    keys = pygame.key.get_pressed()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            # 키가 눌렸을 때
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                print("왼쪽 키 눌림")
-            if event.key == pygame.K_RIGHT:
-                print("오른쪽 키 눌림")
+    ## 캐릭터 움직이기
+    to_x = 0
+    to_y = 0
+    character_speed = 5
 
-            if event.key == pygame.K_UP:
-                print("위로 키 눌림")
-            if event.key == pygame.K_DOWN:
-                print("아래로 키 눌림")
+    keys = pygame.key.get_pressed()
 
-        # 키가 떼졌을 때
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                print("왼쪽 키 떼짐")
-            if event.key == pygame.K_RIGHT:
-                print("오른쪽 키 떼짐")
 
-            if event.key == pygame.K_UP:
-                print("위로 키 떼짐")
-            if event.key == pygame.K_DOWN:
-                print("아래로 키 떼짐")
+    if not SCREEN_WIDTH< character_x_pos < :
+        print('밖으로 나감')
+        to_x = 0
+    else:
+        if keys[pygame.K_UP]:
+            to_y = -5
+            camera.offset.x = -5
+        elif keys[pygame.K_DOWN]:
+            to_y = +5
+            camera.offset.x = +5
+        elif keys[pygame.K_LEFT]:
+            to_x = -5
+            camera.offset.y = -5
+        elif keys[pygame.K_RIGHT]:
+            to_x = +5
+            camera.offset.y = +5
 
-        # 눌린 동안 처리 (계속)
-        if keys[pygame.K_SPACE]:
-            print("스페이스 키 눌림")
+
+    character_x_pos += to_x
+    character_y_pos += to_y
+
+    # 화면에 표시하기
+    SCREEN.blit(background, (0,0))  ## 배경
+    SCREEN.blit(character, (character_x_pos, character_y_pos))  ## 캐릭터
+
+    pygame.display.flip()
+    clock.tick(60)
+
